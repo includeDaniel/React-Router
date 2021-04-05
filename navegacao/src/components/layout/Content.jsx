8eae43c041d7a7723b4110ef936fa6c947cd000b
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-05 19:14:13 -0300

    using tags Switch, Route and Link

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Content.css'
+import {Switch, Route} from 'react-router-dom'
+
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
 
 const Content = props => (
     <main className="Content">
-        <h1>Component Content</h1>
+        <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/home">
+                <Home />
+            </Route>
+        </Switch>
     </main>
 )
 export default Content
--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Menu.css'
 
+import { Link } from 'react-router-dom'
+
 const Menu = props => (
     <aside className="Menu">
         <nav>
             <ul>
                 <li>
-                    <a href="/">Início</a>
+                    <Link to="/home">Início</Link>
                 </li>
                 <li>
-                    <a href="/">Sobre</a>
+                    <Link to="/about">Sobre</Link>
                 </li>
             </ul>
         </nav>
